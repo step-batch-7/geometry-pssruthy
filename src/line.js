cba28d1b0d071736b9dddb8169151181194405e5
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-12 20:46:25 +0530

    Added arePointsEqual method
    What changes you have made?
    --->Added arePointsEqual method.
    --->Changed datastructure to add arePointsEqual method.
    
    Why this change is necessary?
    --->Repetition of code.
    
    What is the impact of the change?
    --->The arePointsEqual method reduced repetition in code.

--- src/line.js
+++ src/line.js
@@ -1,22 +1,29 @@
+const arePointsEqual = function(point1, point2) {
+  let areEqual = point1.x === point2.x;
+  areEqual = areEqual && point1.y === point2.y;
+  return areEqual;
+};
+
 class Line {
-  constructor(x1 = 0, y1 = 0, x2 = 0, y2 = 0) {
-    this.point1 = { x1, y1 };
-    this.point2 = { x2, y2 };
+  constructor(x1, y1, x2, y2) {
+    this.point1 = { x: x1, y: y1 };
+    this.point2 = { x: x2, y: y2 };
   }
 
   toString() {
-    const { x1, y1 } = this.point1;
-    const { x2, y2 } = this.point2;
+    const x1 = this.point1.x;
+    const y1 = this.point1.y;
+    const x2 = this.point2.x;
+    const y2 = this.point2.y;
+
     const pointOne = `End One :${x1},${y1}`;
     const pointTwo = `End Two :${x2},${y2}`;
     return `The representation of the line is:\n${pointOne}\n${pointTwo}`;
   }
-  isEqualTo(newLine) {
-    let isPointOneEqual = this.point1.x1 == newLine.point1.x1;
-    isPointOneEqual = isPointOneEqual && this.point1.y1 == newLine.point1.y1;
 
-    let isPointTwoEqual = this.point2.x2 == newLine.point2.x2;
-    isPointTwoEqual = isPointTwoEqual && this.point2.y2 == newLine.point2.y2;
+  isEqualTo(newLine) {
+    const isPointOneEqual = arePointsEqual(this.point1, newLine.point1);
+    const isPointTwoEqual = arePointsEqual(this.point2, newLine.point2);
 
     return isPointTwoEqual && isPointOneEqual;
   }
--- test/lineTest.js
+++ test/lineTest.js
@@ -9,23 +9,17 @@ describe('Line', function() {
         'The representation of the line is:\nEnd One :1,2\nEnd Two :2,3';
       assert.strictEqual(line.toString(), expectedValue);
     });
-    it('Should give default points when no points are specified', function() {
-      const line = new Line();
-      const expectedValue =
-        'The representation of the line is:\nEnd One :0,0\nEnd Two :0,0';
-      assert.strictEqual(line.toString(), expectedValue);
-    });
   });
 
   describe('isEqualTo', function() {
     it('Should give true when two lines are equal', function() {
       const line = new Line(1, 2, 3, 4);
-      const newLine = { point1: { x1: 1, y1: 2 }, point2: { x2: 3, y2: 4 } };
+      const newLine = { point1: { x: 1, y: 2 }, point2: { x: 3, y: 4 } };
       assert.ok(line.isEqualTo(newLine));
     });
     it('Should give false when two lines are not equal', function() {
       const line = new Line(1, 8, 3, 4);
-      const newLine = { point1: { x1: 1, y1: 2 }, point2: { x2: 3, y2: 4 } };
+      const newLine = { point1: { x: 1, y: 2 }, point2: { x: 3, y: 4 } };
       assert.ok(!line.isEqualTo(newLine));
     });
   });
