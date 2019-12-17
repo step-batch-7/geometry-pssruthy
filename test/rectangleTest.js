8249a8f37c3bc095624e9e28850d564597bbab44
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-17 15:39:04 +0530

    Added test cases for area property of rectangle

--- test/rectangleTest.js
+++ test/rectangleTest.js
@@ -14,5 +14,17 @@ describe('Rectangle', () => {
       const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 6, y: 3 });
       assert.strictEqual(rectangle.area, 10);
     });
+    it('Should give the area of the rectangle when the width is zero', () => {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 6, y: 1 });
+      assert.strictEqual(rectangle.area, 0);
+    });
+    it('Should give the area of the rectangle when the length is zero', () => {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 1, y: 6 });
+      assert.strictEqual(rectangle.area, 0);
+    });
+    it('Should give the area of the rectangle when the length and width are zero', () => {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 1, y: 1 });
+      assert.strictEqual(rectangle.area, 0);
+    });
   });
 });
